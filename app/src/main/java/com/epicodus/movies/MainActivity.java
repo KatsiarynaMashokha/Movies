c24c770178ac967f8846bbdea34099dba4d07fd3
Kate Mashokha and Stephanie Mayer <kate@>
2017-10-09 14:19:04 -0700

    add onClick functionality

--- app/src/main/java/com/epicodus/movies/MainActivity.java
+++ app/src/main/java/com/epicodus/movies/MainActivity.java
@@ -6,8 +6,6 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,7 +13,7 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
      @Bind(R.id.title_text_view) TextView mTitleTextView;
      @Bind(R.id.find_movies_button) Button mFindMoviesButton;
      @Bind(R.id.location_edit_text) EditText mLocationEditText;
@@ -26,17 +24,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Typeface mainActivityFont = Typeface.createFromAsset(getAssets(), "fonts/capture.ttf");
+
         ButterKnife.bind(this);
         mTitleTextView.setTypeface(mainActivityFont);
-        mFindMoviesButton.setOnClickListener(new View.OnClickListener() {
+        mFindMoviesButton.setOnClickListener(this);
+    }
 
-            @Override
-            public void onClick(View v){
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, MoviesActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public void onClick(View v){
+        if(v == mFindMoviesButton){
+        String location = mLocationEditText.getText().toString();
+        Intent intent = new Intent(MainActivity.this, MoviesActivity.class);
+        intent.putExtra("location", location);
+        startActivity(intent);
+        }
+
     }
 }
