916f2589add8ca31654ae0a9d7155655150c0c87
Kate Mashokha and Stephanie Mayer <kate+stephanie@>
2017-10-09 10:05:40 -0700

    add movieActivity

--- app/src/main/java/com/epicodus/movies/MoviesActivity.java
+++ app/src/main/java/com/epicodus/movies/MoviesActivity.java
@@ -1,22 +1,45 @@
 package com.epicodus.movies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MoviesActivity extends AppCompatActivity {
     private TextView mLocationTextView;
     private ListView mListView;
-    private String[] movies = new String[] {"It"};
+    private String[] movies = new String[] {"It", "The Foreigner", "Marshall", "Happy Death Day", "The Secret Scripture"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
 
-//        mListView = (ListView) findViewById(R.id.);
-//        mLocationTextView = (TextView) findViewById(R.id.location_edit_text);
+        mListView = (ListView) findViewById(R.id.listView);
+        mLocationTextView = (TextView) findViewById(R.id.movies_text_view);
+
+        String receivedString = getIntent().getStringExtra("location");
+        mLocationTextView.setText("Movies near " + receivedString);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, movies);
+        mListView.setAdapter(arrayAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String movies = ((TextView)view).getText().toString();
+                Toast.makeText(MoviesActivity.this, movies, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the movies near: " + location);
 
     }
 }
