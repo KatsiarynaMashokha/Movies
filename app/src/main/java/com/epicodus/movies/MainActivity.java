bd98f321b3fd47960dff8f29b1944efc5e22c839
Guest User <Guest@Epicodus-3D.local>
2017-10-09 09:20:10 -0700

    initial setup

---
+++ app/src/main/java/com/epicodus/movies/MainActivity.java
@@ -0,0 +1,17 @@
+package com.epicodus.movies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+     Button mFindMoviesButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
